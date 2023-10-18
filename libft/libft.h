e776b600846e1fd6adad1b443c9876382c5098e3
No.898 <dinh@seznam.cz>
2023-10-18 20:20:15 +0200

    +

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: todinh <todinh@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 16:56:08 by todinh            #+#    #+#             */
-/*   Updated: 2023/10/18 17:05:22 by todinh           ###   ########.fr       */
+/*   Updated: 2023/10/18 20:18:08 by todinh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,12 @@
 # include <stdlib.h>
 
 typedef struct s_list
+{
+    void			*content;
+    struct s_list	*next;
+}	t_list;
+
+#endif
 {
     void			*content;
     struct s_list	*next;
