08c9dec95ca812c9f5b96c824dfc0f431974cdc3
No.898 <dinh@seznam.cz>
2023-10-18 21:17:13 +0200

    +

--- libft/libft.h
+++ libft/libft.h
@@ -5,30 +5,23 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: todinh <todinh@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/18 16:56:08 by todinh            #+#    #+#             */
-/*   Updated: 2023/10/18 20:18:08 by todinh           ###   ########.fr       */
+/*   Created: 2023/10/18 20:49:33 by todinh            #+#    #+#             */
+/*   Updated: 2023/10/18 20:51:47 by todinh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <unistd.h>
 # include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
 
-typedef struct s_list
+typedef struct	s_list
 {
-    void			*content;
-    struct s_list	*next;
-}	t_list;
-
-#endif
-{
-    void			*content;
-    struct s_list	*next;
-
-
-}	t_list;
-
-
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}				t_list;
+int ft_atoi(const char *str)
 #endif
